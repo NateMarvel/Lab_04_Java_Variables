7625525b4896649f519de84baeee51191997c6eb
NateMarvel <marvelnl@mail.uc.edu>
2026-09-19 16:26:45 -0400

    Task 2 Complete

---
+++ src/Task_2.java
@@ -0,0 +1,12 @@
+public class Task_2
+{
+    public static void main(String[] args)
+    {
+        double springCosts = 482.81;
+        double summerCosts = 698.07;
+        double fallCosts = 649.53;
+        double winterCosts = 378.72;
+        double yearlyCosts = springCosts + summerCosts + fallCosts + winterCosts;
+        System.out.println("Your yearly maintenance costs using the values " + springCosts + ", " + summerCosts + ", " + fallCosts + " and " + winterCosts +" is " + yearlyCosts);
+    }
+}
