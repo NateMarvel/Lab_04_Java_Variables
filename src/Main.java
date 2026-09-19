66543748484daf4f3df8015b8006b48abbee51bf
NateMarvel <marvelnl@mail.uc.edu>
2026-09-19 15:37:11 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,35 @@ public class Main
 
     public static void main(String[] args)
     {
-    int OperandA = 1;
-
+    int intOperandA = 1;
+    int intOperandB = 9;
+    int intSum = intOperandA + intOperandB;
+    int intProduct = intOperandA * intOperandB;
+    int intDifference = intOperandA - intOperandB;
+    int intQuotient = intOperandA / intOperandB;
+    int intModulo = intOperandA % intOperandB;
+    double doubleOperandA = 3.50;
+    double doubleOperandB = 16.73;
+    double doubleSum = doubleOperandA + doubleOperandB;
+    double doubleProduct = doubleOperandA * doubleOperandB;
+    double doubleDifference = doubleOperandA - doubleOperandB;
+    double doubleQuotient = doubleOperandA / doubleOperandB;
+    // step 10
+    int numberOfKids = 2;
+    boolean isRaining = true;
+    double priceOfGas = 4.21;
+    int favoriteNumber = 89;
+    double shoeSize = 10.5;
+    String birthMonth = "January";
+    String fullName = "Nathaniel Marvel";
+    System.out.println("the sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+    System.out.println("the product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+    System.out.println("the difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+    System.out.println("the quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient);
+    System.out.println("the modulo using ints of " + intOperandA + " " + intOperandB + " is " + intModulo);
+    System.out.println("the sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+    System.out.println("the product using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct);
+    System.out.println("the difference using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference);
+    System.out.println("the quotient using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient);
     }
-
-
-
-
 }
