40d3b30564309d28bea3092f1073a5827fdafe92
NateMarvel <marvelnl@mail.uc.edu>
2026-09-19 16:51:08 -0400

    Task 4 Complete

---
+++ src/Task_4.java
@@ -0,0 +1,9 @@
+public class Task_4
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 472386;
+        int moduloNumber = numToExamine % 2;
+        System.out.println("If the number stated is zero then your number is even, if it is one then your number is odd. Here's the number: " + moduloNumber + " your number was " + numToExamine);
+    }
+}
